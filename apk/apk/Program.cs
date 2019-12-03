0c4f627ba907d999a3103bfe4c41242b1f7b5e55
詹必炬 <58196619+zhanbiju@users.noreply.github.com>
2019-12-03 11:05:12 +0800

    计算多少只鸡
    计算多少只鸡

---
+++ apk/apk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace apk
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            {
+                int z;
+                for (int x = 0; x < 20; x++)
+                {
+                    for (int y = 0; y < 33; y++)
+                    {
+                        z = 100 - x - y;
+                        if (5 * x + 3 * y + z / 3 == 100 && x+y+z==100)
+                        {
+                            Console.WriteLine("{0} {1} {2}", x, y, z);
+                            Console.ReadLine();
+                        }
+                    }
+                        
+                }
+            }
+        }
+    }
+}
