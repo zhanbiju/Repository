48d23869e3e111772afc6a39a66938553a56f92d
詹必炬 <58196619+zhanbiju@users.noreply.github.com>
2019-12-24 12:11:08 +0800

    计算平均年龄

---
+++ CalculatingTheAverageAge/CalculatingTheAverageAge/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS000.Common
+{
+    /// <summary>
+    /// 人员
+    /// </summary>
+    public class Person
+    {
+        public Guid ID { get; set; }           // 人员识别码
+        public string Name { get; set; }       // 姓名
+        public string Province { get; set; }   // 所在省份
+        public string City { get; set; }       // 城市
+        public bool Sex { get; set; }          // 性别
+        public DateTime Birthday { get; set; } // 出生日期
+        public string Email { get; set; }      // 电子邮件
+
+        public Person()
+        {
+            this.ID = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// 重载，格式化输出人员的基本数据
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var sexString = "男";
+            if (!this.Sex)
+                sexString = "女";
+            var birthday = this.Birthday.ToLongDateString();
+
+            return $"{this.Name} \t{this.Province} \t{this.City} \t{sexString} \t{birthday} \t{this.Email}";
+        }
+
+    }
+}
---
+++ CalculatingTheAverageAge/CalculatingTheAverageAge/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CDS000.Common;
+
+namespace Properties
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var totalDays = 0;  // 所有人员的全部天数
+            var now = DateTime.Now;
+            foreach (var item in PersonRepository.InitialPersonCollection())
+            {
+                totalDays = totalDays + Math.Abs(((TimeSpan)(now - item.Birthday)).Days);
+            }
+
+            var avgTotaldays = totalDays / PersonRepository.InitialPersonCollection().Count();
+            var avgYears = avgTotaldays / 365;
+            var avgDays = avgTotaldays - avgYears * 365;
+            Console.WriteLine("平均年龄：" + avgYears + "周岁" + " - " + avgDays + "天");
+            Console.ReadKey();
+
+
+
+            NameCount(PersonRepository.InitialPersonCollection());
+
+        }
+
+        public static void NameCount(List<Person> people)
+        {
+
+            var v = from i in people
+                    where !i.Name.Contains("欧阳")
+                    group i by i.Name.Substring(0, 1) into c
+                    select new { c.Key, cc = c.Count() };
+            var s = from i in people
+                    where i.Name.Contains("欧阳")
+                    group i by i.Name.Substring(0, 2) into c
+                    select new { c.Key, cc = c.Count() };
+            foreach (var item in v)
+            {
+                Console.WriteLine("姓氏：{0}   {1}", item.Key, item.cc);
+            }
+            foreach (var item in s)
+            {
+                Console.WriteLine("姓氏：{0}   {1}", item.Key, item.cc);
+            }
+        }
+    }
+}
